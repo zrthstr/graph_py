1a21df45ce295e0b2986bb37c916fc2b1a197d0e
zrthstr <zrth1k@gmail.com>
2025-02-02 16:10:47 +0100

    ..

--- age/app.py
+++ age/app.py
@@ -48,22 +48,48 @@ class NetGraph:
         #self.close()
 
     def exist_domain_node(self, domain: DomainNode):
-        ret = self.conn.execCypher("MATCH (n:Domain {host: %s}) RETURN 1 LIMIT 1", params=(domain.host,))
+        ret = self.conn.execCypher("MATCH (d:Domain {host: %s}) RETURN 1 LIMIT 1", params=(domain.host,))
         return bool(ret.fetchone())
 
     def exist_identical_domain_node(self, domain: DomainNode):
-        ret = self.conn.execCypher("MATCH (n:domain {host: %s, source: %s}) RETURN 1 LIMIT 1",params=(domain.host, domain.source))
+        ret = self.conn.execCypher("MATCH (d:Domain {host: %s, source: %s}) RETURN id(d)",params=(domain.host, domain.source))
         return bool(ret.fetchone())
 
     def insert_domain_node(self, domain: DomainNode):
         print("[+] inserting domain_node")
-        ret = self.conn.execCypher("CREATE (d:Domain {host: %s, source: %s}) RETURN id(d)",params=(domain.host,domain.source))
+        self.ifnonex_insert_domain_root_node( domain)
+        #ret = self.conn.execCypher("CREATE (d:Domain {host: %s, source: %s}) RETURN id(d)",params=(domain.host,domain.source))
+        #ret_id = ret.fetchone()
+
+
+        query = """
+            MATCH (r:DomainRoot {host: %s})
+            CREATE (d:Domain {host: %s, source: %s})
+            CREATE (r)-[:CONNECTED_TO]->(d)
+            RETURN id(d)
+            """
+        ret = self.conn.execCypher(query, params=(domain.input, domain.host, domain.source))
         ret_id = ret.fetchone()
         print(f"[+] inserted with id {ret_id}")
 
-        ###### I just do not fucking know if i need to commit
-        #self.conn.commit()
+        self.conn.commit()
         return ret_id
+
+    def exists_domain_root_node(self, domain: DomainNode):
+        ret = self.conn.execCypher("MATCH (r:DomainRoot {host: %s}) RETURN id(r)", params=(domain.input,))
+        return bool(ret.fetchone())
+
+    def ifnonex_insert_domain_root_node(self, domain: DomainNode):
+        print("[+] inserting domain root node if nonexistant")
+        if self.exists_domain_root_node(domain):
+            print("[i] domain root node found, skipping")
+            return
+        ret = self.conn.execCypher("CREATE (r:DomainRoot {host: %s}) RETURN id(r)", params=(domain.input,))
+        ret_id = ret.fetchone()
+        self.conn.commit()
+        print(f"[+] inserted new DomainRoot {ret_id}")
+        return ret_id
+
 
     def count_domain_node(self):
         ret = self.conn.execCypher("MATCH (n:Domain) RETURN COUNT(DISTINCT n.host) ")
@@ -74,15 +100,36 @@ class NetGraph:
         if self.exist_domain_node(domain):
             if self.exist_identical_domain_node(domain):
                 print("[i] domain node exists with same attribiutes")
-                # noting to do
                 return
             else:
                 print("[i] domain node exsists but with other attribiutes")
-                # update source  // merge domain nodes
+                raise Exception("Not implemented")
+                ## merge nodes!
                 return
         print("[i] domain_node not found")
         self.insert_domain_node(domain)
         return
+
+    def get_all_domain_nodes_and_rel(self):
+        query = """
+        MATCH (r:DomainRoot)-[:CONNECTED_TO]->(d:Domain)
+        RETURN r.host AS root_name, d.host AS domain_host
+        """
+        pass
+
+    def dump_nodes(self):
+        query = "MATCH (n) RETURN n"
+        ret = self.conn.execCypher(query)
+        return ret.fetchall()
+
+    def dump_nodes_with_rel(self):
+
+        query = """
+        MATCH (r:DomainRoot)-[:CONNECTED_TO]->(d:Domain)
+        RETURN r.host, d.host
+        """
+        cursor = self.conn.execCypher(query, cols=["root_name VARCHAR", "domain_host VARCHAR"])
+        return cursor.fetchall()  # Returns list of (root_name, domain_host)
 
 
 if __name__ == "__main__":
@@ -95,8 +142,13 @@ if __name__ == "__main__":
         #ng.insert_domain_node(domain_node)
         ng.sync_domain_node(domain_node)
 
+    #a = ng.get_all_domain_nodes_and_rel()
+
+    print(ng.dump_nodes())
+    print("-" * 12)
+    print(ng.dump_nodes_with_rel())
 
     print("[i] Number of domains: ", ng.count_domain_node())
 
-    ng.delete()
+    #ng.delete()
     ng.close()
